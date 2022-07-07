4c2b03ffb710ce547433573be446106ef1c75fb7
wyoung163 <20201014@sungshin.ac.kr>
2022-07-07 23:27:00 +0900

    초기 세팅

---
+++ src/controllers/userController.js
@@ -0,0 +1,21 @@
+import { UserService } from "../services/userService.js";
+
+const userController = {
+  // 회원가입
+  createUser: async (req, res, next) => {
+
+  },
+  
+  // 회원 정보 조회
+  findUserInfo: async (req, res, next) => {
+
+  },
+
+  // 회원 정보 수정
+  updateUserInfo: async (req, res, next) => { 
+   
+  },
+};
+
+
+export { userController };
