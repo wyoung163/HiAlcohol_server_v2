06d5c2af789f8115d7e08f3043410d57f371eeef
wyoung163 <20201014@sungshin.ac.kr>
2022-07-09 16:54:50 +0900

    modify: 사용자 로그인 여부 확인 수정

--- src/routes/suggestionRouter.js
+++ src/routes/suggestionRouter.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { loginRequired } from "../middlewares/loginRequired.js";
 import { showSuggestions, showSuggestionBoard, addSuggestionBoard, editSuggestionBoard } from "../controllers/suggestionController.js";
 import { suggestionValidator } from "../middlewares/express-validator/index.js";
 
@@ -6,7 +7,7 @@ const suggestionRouter = Router();
 
 suggestionRouter.get("/suggestions", showSuggestions);
 suggestionRouter.get("/suggestions/:id", showSuggestionBoard);
-suggestionRouter.post("/suggestion", suggestionValidator.checkSuggestion, addSuggestionBoard);
-suggestionRouter.patch("/suggestions/:id", suggestionValidator.checkSuggestion, editSuggestionBoard);
+suggestionRouter.post("/suggestion", loginRequired, suggestionValidator.checkSuggestion, addSuggestionBoard);
+suggestionRouter.patch("/suggestions/:id", loginRequired, suggestionValidator.checkSuggestion, editSuggestionBoard);
 
 export { suggestionRouter };
