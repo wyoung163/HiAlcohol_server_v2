a108afafc07d1b09fa369556b6ce256626632d69
wyoung163 <20201014@sungshin.ac.kr>
2022-07-20 10:24:09 +0900

    modify: swagger 추가

--- src/routes/likeRouter.js
+++ src/routes/likeRouter.js
@@ -12,26 +12,26 @@ const likeRouter = Router();
  */
 /**
  * @swagger
- * /:
- *   get:
+ * /board/:id/like:
+ *   post:
  *    summary: 꿀조합 게시판 좋아요 선택 API
  *    description: 꿀조합 게시글에 대한 좋아요를 선택하는 API입니다.
- *    tags: [Home]
+ *    tags: [Like]
  *    parameters:
- *    - name: 
- *      in: 
- *      description: 
- *      required: 
+ *    - name: id
+ *      in: params
+ *      description: 파라미터에 꿀조합 게시글 아이디를 입력하세요.
+ *      required: true
  *      schema:
  *        type: 
  *      examples:
  *        Sample:
- *          value: 
- *          summary: 
- *      style: 
+ *          value: example value
+ *          summary: id
+ *      style: simple
  *    responses:
  *      200:
- *        description: 메인 홈 화면(오늘의 술 추천) 조회
+ *        description: 꿀조합 게시판 좋아요 선택 
  *        content:
  *          application/json:
  *            schema:
@@ -42,23 +42,130 @@ const likeRouter = Router();
  *                  example: 200
  *                message:
  *                  type: string
- *                  example: 좋아요 선택에 성공하였습니다..
+ *                  example: 좋아요 선택에 성공하였습니다.
  *                data:
  *                  type: object
  *                  properties:
- *                    id:
+ *                    likeId:
  *                      type: number
- *                      description: 칵테일 아이디
- *                      example: 1, 2, 3, 4, 5, ...
- *                    cocktail:
- *                      type: string
- *                      description: 칵테일 이름
- *                      example: 블랙 러시안, 화이트 러시안, ... 
+ *                      description: 추가된 좋아요 아이디
+ *                      example: 1 
  */
 likeRouter.post("/board/:id/like", loginRequired, addBoardLike);
+
+/**
+ * @swagger
+ * /board/:id/like:
+ *   delete:
+ *    summary: 꿀조합 게시판 좋아요 취소 API
+ *    description: 꿀조합 게시글에 대한 좋아요를 취소하는 API입니다.
+ *    tags: [Like]
+ *    parameters:
+ *    - name: id
+ *      in: params
+ *      description: 파라미터에 꿀조합 게시글 아이디를 입력하세요.
+ *      required: true
+ *      schema:
+ *        type: 
+ *      examples:
+ *        Sample:
+ *          value: example value
+ *          summary: id
+ *      style: simple
+ *    responses:
+ *      200:
+ *        description: 꿀조합 게시판 좋아요 취소
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                code:
+ *                  type: number
+ *                  example: 200
+ *                message:
+ *                  type: string
+ *                  example: 좋아요 취소에 성공하였습니다.
+ */
 likeRouter.delete("/board/:id/like", loginRequired, cancelBoardLike);
 
+/**
+ * @swagger
+ * /suggestion/:id/like:
+ *   post:
+ *    summary: 건의 게시판 좋아요 선택 API
+ *    description: 건의 게시글에 대한 좋아요를 선택하는 API입니다.
+ *    tags: [Like]
+ *    parameters:
+ *    - name: id
+ *      in: params
+ *      description: 파라미터에 건의 게시글 아이디를 입력하세요.
+ *      required: true
+ *      schema:
+ *        type: 
+ *      examples:
+ *        Sample:
+ *          value: example value
+ *          summary: id
+ *      style: simple
+ *    responses:
+ *      200:
+ *        description: 건의 게시판 좋아요 선택 
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                code:
+ *                  type: number
+ *                  example: 200
+ *                message:
+ *                  type: string
+ *                  example: 좋아요 선택에 성공하였습니다.
+ *                data:
+ *                  type: object
+ *                  properties:
+ *                    likeId:
+ *                      type: number
+*                       description: 추가된 좋아요 아이디
+ *                      example: 1, 2, 3, ...
+ */
 likeRouter.post("/suggestion/:id/like", loginRequired, addSuggestionLike);
+
+/**
+ * @swagger
+ * /suggestion/:id/like:
+ *   delete:
+ *    summary: 건의 게시판 좋아요 취소 API
+ *    description: 건의 게시글에 대한 좋아요를 취소하는 API입니다.
+ *    tags: [Like]
+ *    parameters:
+ *    - name: id
+ *      in: params
+ *      description: 파라미터에 건의 게시글 아이디를 입력하세요.
+ *      required: true
+ *      schema:
+ *        type: 
+ *      examples:
+ *        Sample:
+ *          value: example value
+ *          summary: id
+ *      style: simple
+ *    responses:
+ *      200:
+ *        description: 건의 게시판 좋아요 취소
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                code:
+ *                  type: number
+ *                  example: 200
+ *                message:
+ *                  type: string
+ *                  example: 좋아요 취소에 성공하였습니다.
+ */
 likeRouter.delete("/suggestion/:id/like", loginRequired, cancelSuggestionLike);
 
 export { likeRouter };
