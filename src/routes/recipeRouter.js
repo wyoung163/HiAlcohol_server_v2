f9708c609f7da070e59fbe1e49e5a9b896124bee
wyoung163 <20201014@sungshin.ac.kr>
2022-07-28 21:27:38 +0900

    modify: recipe swagger body 변경

--- src/routes/recipeRouter.js
+++ src/routes/recipeRouter.js
@@ -99,17 +99,6 @@ recipeRouter.post("/admin/recipe", loginRequired, isAdmin, recipeValidator.check
  *    description: 술 레시피 수정에 대한 API입니다.
  *    tags: [Recipe]
  *    parameters:
- *    - name: id
- *      in: body
- *      description: body에 레시피 id를 입력하세요
- *      required: true
- *      schema:
- *        type: int
- *      examples:
- *        Sample:
- *          value: 2
- *          summary: id
- *      style: simple
  *    - name: cocktailInfo
  *      in: body
  *      description: body에 칵테일 정보를 입력하세요
@@ -117,6 +106,10 @@ recipeRouter.post("/admin/recipe", loginRequired, isAdmin, recipeValidator.check
  *        type: object
  *        required: true
  *        properties:
+ *           id:
+ *             type: int
+ *             description: 칵테일 아이디
+ *             example: 50
  *           cocktail:
  *             type: string
  *             description: 칵테일 이름
