5978f33ef33b465a28b81009b293b29b90d29fc1
Jiwoo An <jiyouan9@gmail.com>
2022-07-31 22:14:42 +0900

    fix: userData 수정

--- src/services/userService.js
+++ src/services/userService.js
@@ -65,7 +65,7 @@ const UserService = {
 
     // 로그인 성공 -> JWT 웹 토큰 생성
     const secretKey = process.env.JWT_SECRET_KEY || "jwt-secret-key";
-    const token = jwt.sign({ id: user.id }, secretKey);
+    const token = jwt.sign({ id: userData.id }, secretKey);
 
     const loginUser = {
       id,
