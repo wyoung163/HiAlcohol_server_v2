96dcfcddba7d9bd14a5b4ed4e76d9d0c63b1493d
Jiwoo An <jiyouan9@gmail.com>
2022-08-15 23:25:26 +0900

    modify: ssl 설정중

--- index.js
+++ index.js
@@ -59,6 +59,6 @@ const httpsServer = https.createServer(credentials, app);
 // 	console.log('HTTP Server running on port 80');
 // });
 
-httpsServer.listen(443, () => {
+app.listen(443, () => {
 	console.log('HTTPS Server running on port 443');
 });
